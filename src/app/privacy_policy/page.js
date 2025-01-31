e0df8e073d05e7d905638a3f6cd5728187773ffa
rabby khan <rabbykhan082020@gmail.com>
2025-01-31 03:12:38 -0500

    privacy policy page added for  testing

---
+++ src/app/privacy_policy/page.js
@@ -0,0 +1,9 @@
+const Page = async () => {
+    return (
+      <div className="flex w-full">  
+      
+        <h1 className="flex justify-center p-10">We use email, google user Id and the name only.... </h1>
+      </div>
+    );
+  };
+  export default Page;
